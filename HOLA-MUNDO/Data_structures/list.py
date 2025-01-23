2d63993e16833e3806bc233b64aa1008b5d8bcc1
jcamilop <juank01102114@gmail.com>
2025-01-22 23:32:20 -0500

    changes

---
+++ HOLA-MUNDO/Data_structures/list.py
@@ -0,0 +1,3 @@
+
+colors = ['red', 'green', 'blue', 'yellow', 'black'] #las listas se definen por [] y son mutables
+print(colors)
---
+++ HOLA-MUNDO/Data_structures/tupla.py
@@ -0,0 +1,2 @@
+nombres = ('camilo', 'carlos', 'pedro') # las tuplas se definen con () y no son mutables
+print(nombres)
